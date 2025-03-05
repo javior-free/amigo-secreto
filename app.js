774f60cf1c9b633cbd74f489db99d2968c751aec
Javier Orellana <javisda@gmail.com>
2025-03-05 13:17:31 -0300

    actualiza el app

--- app.js
+++ app.js
@@ -25,3 +25,17 @@ function renderizarAmigo(){
 
     }
 }
+
+function sortearAmigo() {
+    if(amigo.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    let amigoSorteado = amigo [Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML = "";
+}
